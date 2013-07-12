54a50f8148a73d90270d778b94566b0deab535ad
jonathan <jonathan@kyuss.org>
2013-07-12 16:36:20 -0700

    simplify shell script

--- s3s3mirror.sh
+++ s3s3mirror.sh
@@ -4,19 +4,17 @@ THISDIR=$(dirname $0)
 cd ${THISDIR}
 THISDIR=$(pwd)
 
-DEBUG=$1
-if [ "x${DEBUG}" = "x" ] ; then
-  # This will cause an error, required args missing, but let the code tell them what the problem is
-  java -jar target/s3s3mirror-1.0.0-SNAPSHOT.jar "$@"
+JARFILE=target/s3s3mirror-1.0.0-SNAPSHOT.jar
 
-elif [ "${DEBUG}" = "--debug" ] ; then
+DEBUG=$1
+if [ "${DEBUG}" = "--debug" ] ; then
   # Run in debug mode
   shift   # remove --debug from options
-  java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 -jar target/s3s3mirror-1.0.0-SNAPSHOT.jar "$@"
+  java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 -jar ${JARFILE} "$@"
 
 else
   # Run in regular mode
-  java -jar target/s3s3mirror-1.0.0-SNAPSHOT.jar "$@"
+  java -jar ${JARFILE} "$@"
 fi
 
 exit $?
