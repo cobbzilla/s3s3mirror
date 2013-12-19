9ff76494a912a4345cfe739cea29af1ce6874cc0
jonathan <jonathan@kyuss.org>
2013-12-19 15:29:14 -0800

    simplify

--- src/main/java/org/cobbzilla/s3s3mirror/MirrorContext.java
+++ src/main/java/org/cobbzilla/s3s3mirror/MirrorContext.java
@@ -1,15 +1,13 @@
 package org.cobbzilla.s3s3mirror;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 public class MirrorContext {
 
     @Getter @Setter private MirrorOptions options;
-    @Getter @Setter private MirrorStats stats = new MirrorStats();
-
-    public MirrorContext (MirrorOptions options) {
-        this.options = options;
-    }
+    @Getter private final MirrorStats stats = new MirrorStats();
 
 }
