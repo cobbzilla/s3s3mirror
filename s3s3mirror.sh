4443d0464d226bba377c13fbf755131dca93a87a
jonathan <jonathan@kyuss.org>
2014-12-02 10:37:31 -0800

    better handling for directory paths that contain whitespace

--- s3s3mirror.sh
+++ s3s3mirror.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-THISDIR=$(cd $(dirname $0) && pwd)
+THISDIR=$(cd "$(dirname $0)" && pwd)
 
 VERSION=2.0.1
-JARFILE=${THISDIR}/target/s3s3mirror-${VERSION}-SNAPSHOT.jar
+JARFILE="${THISDIR}/target/s3s3mirror-${VERSION}-SNAPSHOT.jar"
 VERSION_ARG="-Ds3s3mirror.version=${VERSION}"
 
 DEBUG=$1
 if [ "${DEBUG}" = "--debug" ] ; then
   # Run in debug mode
   shift   # remove --debug from options
-  java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 ${VERSION_ARG} -jar ${JARFILE} "$@"
+  java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 ${VERSION_ARG} -jar "${JARFILE}" "$@"
 
 else
   # Run in regular mode
-  java ${VERSION_ARG} -jar ${JARFILE} "$@"
+  java ${VERSION_ARG} -jar "${JARFILE}" "$@"
 fi
 
 exit $?
