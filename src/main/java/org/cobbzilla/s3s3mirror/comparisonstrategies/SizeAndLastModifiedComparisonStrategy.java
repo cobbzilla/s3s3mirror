0d1dfbe283c963913ccb210820825319a58a0b5f
Kyle Carter <kcarter@mastercontrol.com>
2021-01-19 14:10:17 -0700

    Added tests and fixed small bug exposed by test.

--- src/main/java/org/cobbzilla/s3s3mirror/comparisonstrategies/SizeAndLastModifiedComparisonStrategy.java
+++ src/main/java/org/cobbzilla/s3s3mirror/comparisonstrategies/SizeAndLastModifiedComparisonStrategy.java
@@ -6,6 +6,6 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 public class SizeAndLastModifiedComparisonStrategy extends SizeOnlyComparisonStrategy {
     @Override
     public boolean sourceDifferent(S3ObjectSummary source, ObjectMetadata destination) {
-        return super.sourceDifferent(source, destination) || source.getLastModified().after(source.getLastModified());
+        return super.sourceDifferent(source, destination) || source.getLastModified().after(destination.getLastModified());
     }
 }
