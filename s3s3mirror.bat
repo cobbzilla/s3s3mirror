b1e34e368d8521d429affcbd16c9c84992a360e0
Kyle Carter <kcarter@mastercontrol.com>
2021-01-19 14:12:34 -0700

    Upped version number and introduced mockito for testing.

--- s3s3mirror.bat
+++ s3s3mirror.bat
@@ -1,2 +1,2 @@
 @echo off
-java -Dlog4j.configuration=file:target/classes/log4j.xml -Ds3s3mirror.version=1.2.7 -jar target/s3s3mirror-1.2.7-SNAPSHOT.jar %*
+java -Dlog4j.configuration=file:target/classes/log4j.xml -Ds3s3mirror.version=1.2.8 -jar target/s3s3mirror-1.2.7-SNAPSHOT.jar %*
