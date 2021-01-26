96edd45a80c995c0677bb23a0c840ff056f76263
Kyle Carter <kcarter@mastercontrol.com>
2021-01-26 15:32:35 -0700

    Incremented Version

--- s3s3mirror.sh
+++ s3s3mirror.sh
@@ -3,7 +3,7 @@
 
 THISDIR=$(cd "$(dirname $0)" && pwd)
 
-VERSION=2.1.5
+VERSION=2.1.6
 JARFILE="${THISDIR}/target/s3s3mirror-${VERSION}-SNAPSHOT.jar"
 VERSION_ARG="-Ds3s3mirror.version=${VERSION}"
 
